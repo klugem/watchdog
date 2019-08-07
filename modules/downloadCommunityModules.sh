84a30d2df5d5e9218e3724c056cd52ea87ab5457
Michael Kluge <michael.kluge@bio.ifi.lmu.de>
2019-08-07 14:41:34 +0200

    updated module download script

--- modules/downloadCommunityModules.sh
+++ modules/downloadCommunityModules.sh
@@ -17,27 +17,104 @@ if [ -d "${MOVE_DIR}" ]; then
 	exit 0
 fi
 
-confirm "Do you really want to move the content of '${TARGET_DIR}' to '${MOVE_DIR}' and download all modules stored in the community repository?"
-if [ $CONFIRM_RETURN -eq 1 ]; then
-	confirm "Are you sure?!"
+# get MODE to work with
+MODE=$(getInput "Do you want to download all (a) or only selected modules (s)? ")
+case "$MODE" in
+	s)
+	MODE="s"
+	;;
+	a)
+	MODE="a"
+	;;
+	*)
+	echoError "Only 'a' or 's' is valid as input!"
+	exit 1
+	;;
+esac
 
+# move all modules
+if [ "${MODE}" == "a" ]; then
+	confirm "Do you really want to move the content of '${TARGET_DIR}' to '${MOVE_DIR}' and download all modules stored in the community repository?"
 	if [ $CONFIRM_RETURN -eq 1 ]; then
-		mkdir -p "${MOVE_DIR}"
-		for I in `ls -d ${TARGET_DIR}/*/`; do		
-			mv -i "${I}" "${MOVE_DIR}"
-		done
-		echoInfo "Moved all modules into '${MOVE_DIR}'."
-		OK=1
+		confirm "Are you sure?!"
+
+		if [ $CONFIRM_RETURN -eq 1 ]; then
+			mkdir -p "${MOVE_DIR}"
+			for I in `ls -d ${TARGET_DIR}/*/`; do		
+				mv -i "${I}" "${MOVE_DIR}"
+			done
+			echoInfo "Moved all modules into '${MOVE_DIR}'."
+			OK=1
+		fi
 	fi
+else
+	OK=1
 fi
 
 # start with download
+echo "Downloading modules..."
 cd "${TARGET_DIR}"
 if [ ${OK} -eq 1 ]; then
 	wget -qO- -O "${DOWNLOAD_FILE}" "${URL}" 
 	unzip -q -d "${EXTRACT_DIR}" "${DOWNLOAD_FILE}"
 	rm "${DOWNLOAD_FILE}"
-	mv "${EXTRACT_DIR}/${GIT_NAME_IN_ARCHIVE}/"* .
+	if [ "${MODE}" == "a" ]; then
+		mv "${EXTRACT_DIR}/${GIT_NAME_IN_ARCHIVE}/"* .
+	else 
+		# let user decide which should be moved
+		COUNT=1
+		for M in `ls -d ${EXTRACT_DIR}/${GIT_NAME_IN_ARCHIVE}/*/`; do
+			B=$(basename "${M}")
+			EXIST="[NEW]"
+			# test if folder exists
+			if [ -d "${TARGET_DIR}/${B}" ]; then 
+				EXIST="[EXISTS]"
+			fi
+			echo "${COUNT}: ${B} ${EXIST}"
+			COUNT=${COUNT}+1
+		done
+
+		NUMBERS=$(getInput "Please enter the number of modules you want to install (format: 1,2,3-5): ")
+		confirm "Do you really want to install modules with the numbers '${NUMBERS}' and move module duplicates in '${TARGET_DIR}' to '${MOVE_DIR}'?"
+		mkdir -p "${MOVE_DIR}"
+
+		if [ $CONFIRM_RETURN -eq 1 ]; then
+			# get numbers
+			IFS=',' read -r -a array <<< "${NUMBERS}"
+			USE=()
+			for N in "${array[@]}"; do
+				if [[ "${N}" =~ ^[0-9]+-[0-9]+$ ]]; then
+					IFS='-' read -ra ST_EN <<< "${N}"
+					USE+=($(seq "${ST_EN[0]}" "${ST_EN[1]}"))
+				else
+					USE+=("${N}")
+				fi
+			done
+			# sort array
+			IFS=$'\n' USE=($(sort <<<"${USE[*]}"))
+	
+			# copy or move
+			COUNT=1
+			INDEX=0
+			for M in `ls -d ${EXTRACT_DIR}/${GIT_NAME_IN_ARCHIVE}/*/`; do
+
+				if [ ${USE[${INDEX}]} -eq $COUNT ] && [ $INDEX -lt ${#USE[@]} ]; then
+					B=$(basename "${M}")
+					# test if folder exists
+					if [ -d "${TARGET_DIR}/${B}" ]; then 
+						mv "${TARGET_DIR}/${B}" "${MOVE_DIR}/${B}"
+						echoInfo "Moved '"${TARGET_DIR}/${B}"' into '${MOVE_DIR}${B}'."
+					fi
+
+					mv "${EXTRACT_DIR}/${GIT_NAME_IN_ARCHIVE}/${B}" "${TARGET_DIR}/"
+					echoInfo "Installed ${B}..."
+					INDEX=${INDEX}+1
+				fi
+				COUNT=${COUNT}+1
+			done
+		fi
+
+	fi
 	rm -r "${EXTRACT_DIR}/"
 fi
 
