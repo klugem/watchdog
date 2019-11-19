ae6f0eb48a37605862a60237730725c05b319b53
Michael Kluge <michael.kluge@bio.ifi.lmu.de>
2019-11-19 13:23:06 +0100

    fixed typo in docu; added ignore 'flag' to dependency test

--- helper_scripts/dependencyTest.sh
+++ helper_scripts/dependencyTest.sh
@@ -9,6 +9,7 @@ for MODULE in `find modules -maxdepth 3 -name "*.sh" 2>/dev/null | grep -v "test
 do
 	HAS_DEP=0
 	MOD=$(echo "$MODULE" | sed 's|modules/||' | sed -E 's|/.+||')
+
 	VAR=$(grep -E '^USED_TOOLS=' "$MODULE" | sed 's/\W*USED_TOOLS=//' | sed "s/['\"]//g" | grep -v '!TODO!' | sort -u | grep -E -v "^\^" | tr ':' '\n' | sort -u | tr '\n' ':' | sed 's/::/:/g' | sed -E 's/^://' | sed -E 's/:$//' | sed -E 's/\W+#.+$//')
 	if [ "$VAR" != "" ]; then
 		echo "Checking binaries for '$MODULE'..."
@@ -46,22 +47,24 @@ do
 	# check for R packages
 	VAR=$(grep -E '^USED_TOOLS=' "$MODULE" | sed 's/\W*USED_TOOLS=//' | sed "s/['\"]//g" | grep -v '!TODO!' | sort -u | grep "^\^R" | tr '|' '\n')
 	# automatically find libs that are only mentioned in the R files
-	cd "$SCRIPT_FOLDER/../modules/$MOD"
-	VAR="$VAR|"$(find . -maxdepth 2 -name "*.R" -exec grep -o -E '(library|require)\([^)\\$]+\)' {} \; | sed 's/library(//' | sed 's/require(//' | sed 's/)//' | sed 's/"//g' | sed "s/'//g")
-	VAR=$(echo "$VAR" | tr '|' '\n' | sort -u | tr '\n' '|' | sed -E 's/\^R//g' | sed 's/||/|/g' | sed -E 's/^\|//' | sed -E 's/\|$//')
-	cd "$SCRIPT_FOLDER/.."
-	if [ "$VAR" != "" ]; then
-		echo "Checking installed R packages for '$MODULE'..."
-		MESSAGE=$("$SCRIPT_FOLDER/../core_lib/checkUsedTools.sh" "^R|$VAR")
-		CODE=$?
-		HAS_DEP=1
+	if [ -d "$SCRIPT_FOLDER/../modules/$MOD" ]; then
+		cd "$SCRIPT_FOLDER/../modules/$MOD"
+		VAR="$VAR|"$(find . -maxdepth 2 -name "*.R" -exec grep -v "#NO_CHECK" {} \; | grep -o -E '(library|require)\([^)\\$]+\)' | sed 's/library(//' | sed 's/require(//' | sed 's/)//' | sed 's/"//g' | sed "s/'//g")  
+		VAR=$(echo "$VAR" | tr '|' '\n' | sort -u | tr '\n' '|' | sed -E 's/\^R//g' | sed 's/||/|/g' | sed -E 's/^\|//' | sed -E 's/\|$//')
+		cd "$SCRIPT_FOLDER/.."
+		if [ "$VAR" != "" ]; then
+			echo "Checking installed R packages for '$MODULE'..."
+			MESSAGE=$("$SCRIPT_FOLDER/../core_lib/checkUsedTools.sh" "^R|$VAR")
+			CODE=$?
+			HAS_DEP=1
 
-		if [ $CODE -ne 0 ]; then
-			echoError "$MESSAGE"
-			MISSING=1
-			INACTIVE="$MOD:$INACTIVE"
-		else
-			OK_MOD="$MOD:$OK_MOD"
+			if [ $CODE -ne 0 ]; then
+				echoError "$MESSAGE"
+				MISSING=1
+				INACTIVE="$MOD:$INACTIVE"
+			else
+				OK_MOD="$MOD:$OK_MOD"
+			fi
 		fi
 	fi
 
